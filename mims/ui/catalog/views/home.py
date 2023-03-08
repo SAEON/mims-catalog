31d3f2188b932710fbb877614efcdfc3e8f76c52
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2023-03-08 09:58:12 +0200

    Add homepage quick search links

--- mims/ui/catalog/views/home.py
+++ mims/ui/catalog/views/home.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+
 from flask import Blueprint, render_template
 
 from odp.ui.base.forms import SearchForm
 
-bp = Blueprint('home', __name__)
+bp = Blueprint(
+    'home', __name__,
+    static_folder=Path(__file__).parent.parent / 'static',
+    static_url_path='/static/mims',
+)
 
 
 @bp.route('/')
