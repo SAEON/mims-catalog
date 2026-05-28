9ab5a815e33365e18f84495f4782f0ace323d772
dylan <d.pivo@saeon.nrf.ac.za>
2026-05-28 13:59:44 +0200

    mims data submission additions

--- mims/ui/catalog/views/__init__.py
+++ mims/ui/catalog/views/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 
 def init_app(app: Flask):
     from . import home
-    from odp.ui.base.views import catalog
+    from odp.ui.base.views import catalog, submissions
 
     app.register_blueprint(home.bp)
     app.register_blueprint(catalog.bp, url_prefix='/catalog')
+    app.register_blueprint(submissions.bp, url_prefix='/submissions')
